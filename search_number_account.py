e16401ec6c4cf195351611c768de38383bde0825
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-19 21:51:55 +0200

    Update

--- search_number_account.py
+++ search_number_account.py
@@ -1,16 +1,71 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import csv
 
-def find_account_number_in_csv(account_number, filename):
+# Tworzenie silnika bazy danych
+engine = create_engine('sqlite:///Customers.db', echo=True)
+
+# Inicjalizacja klasy bazowej
+Base = declarative_base()
+
+# Definicja modelu tabeli
+class Customer(Base): 
+    __tablename__ = 'Customers'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    number_phone = Column(String) 
+    e_mail = Column(String)  
+    number_bank_statement = Column(String)  
+    
+# Tworzenie tabeli w bazie danych
+Base.metadata.create_all(engine)
+
+# Tworzenie klasy sesji
+Session = sessionmaker(bind=engine)
+
+def get_customer_bank_account(customer_id):
+    session = Session()
+    customer = session.query(Customer).filter_by(id=customer_id).first()
+    session.close()
+    if customer:
+        return customer.number_bank_statement
+    else:
+        return None
+
+def find_transactions_with_account_number(account_number, filename):
+    found_rows = []
     with open(filename, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
+        header = next(reader)  # Pomijamy nagłówek
         for row in reader:
-            if account_number in row:
-                amount = row[-3]
-                currency = row[-1]
-                print(f"Kwota: {amount} {currency}")
+            full_row_text = ' '.join(row)  # Łączymy wszystkie pola w wierszu
+            if account_number in full_row_text:
+                found_rows.append(', '.join(row))
+    return found_rows
 
-# Example usage
-account_to_find = '68 1050 1230 1000 0090 3037 2263'  #numer konta który jest dłużnikiem 
-csv_file = 'bank_statements.csv' #nazwa pliku CSV 
-find_account_number_in_csv(account_to_find, csv_file)
+def main():
+    csv_file = 'bank_statements.csv'
 
+    session = Session()
+    customers = session.query(Customer).all()
+    session.close()
+
+    if not customers:
+        print("Brak dostępnych klientów.")
+        return
+
+    for customer in customers:
+        account_number = customer.number_bank_statement
+        found_rows = find_transactions_with_account_number(account_number, csv_file)
+
+        if not found_rows:
+            print(f"Nie znaleziono żadnych operacji dla klienta o ID: {customer.id}")
+        else:
+            print(f"Operacje dla klienta o ID: {customer.id} i numerze konta bankowego: {account_number}")
+            for row in found_rows:
+                print(row)
+
+if __name__ == "__main__":
+    main()
