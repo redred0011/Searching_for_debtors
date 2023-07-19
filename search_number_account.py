c76d7f8dc05c83e3a7acb01947530ce6f5124a33
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-19 22:11:53 +0200

    Update

--- search_number_account.py
+++ search_number_account.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import csv
-
+import sending_mail
 # Tworzenie silnika bazy danych
 engine = create_engine('sqlite:///Customers.db', echo=True)
 
@@ -56,16 +56,25 @@ def main():
         print("Brak dostępnych klientów.")
         return
 
+    accounts_without_transactions = []  # Lista przechowująca numery kont, dla których nie znaleziono operacji
+
     for customer in customers:
         account_number = customer.number_bank_statement
         found_rows = find_transactions_with_account_number(account_number, csv_file)
 
         if not found_rows:
-            print(f"Nie znaleziono żadnych operacji dla klienta o ID: {customer.id}")
+            print(f"Nie znaleziono żadnych operacji dla klientów:")
+            accounts_without_transactions.append(account_number)
         else:
-            print(f"Operacje dla klienta o ID: {customer.id} i numerze konta bankowego: {account_number}")
+            print(f"Operacje dla klienta o ID: {customer.id}, nazwie: {customer.name} i numerze konta bankowego: {account_number}")
             for row in found_rows:
                 print(row)
+
+    if accounts_without_transactions:
+        print("Operacja dla tego kontrahenta nie została znaleziona:")
+        for account_number in accounts_without_transactions:
+             print(f"Id: {customer.id}, Nazwa: {customer.name}, Numer rachunku: {account_number}")
+             
 
 if __name__ == "__main__":
     main()
