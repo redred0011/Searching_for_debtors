77107347028b8f0d2c6efb7cc0d7e1bb44a0eb7c
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-20 22:14:56 +0200

    Update

--- sending_mail.py
+++ sending_mail.py
@@ -3,6 +3,29 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 import docx
+import configparser
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Tworzenie bazy danych i tabeli dla Debtors
+debtors_engine = create_engine('sqlite:///debtors.db')
+BaseDebtors = declarative_base()
+
+class Debtor(BaseDebtors):
+    __tablename__ = 'Debtors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    number_phone = Column(String)
+    e_mail = Column(String)
+    number_bank_statement = Column(String)
+
+# Tworzenie tabeli w bazie danych dla Debtors
+BaseDebtors.metadata.create_all(debtors_engine)
+
+# Tworzenie klasy sesji dla Debtors
+SessionDebtors = sessionmaker(bind=debtors_engine)
 
 def send_email(sender_email, sender_password, recipient_email, subject, body):
     try:
@@ -35,13 +58,38 @@ def send_email(sender_email, sender_password, recipient_email, subject, body):
     except Exception as e:
         print(f"Błąd podczas wysyłania wiadomości email: {e}")
 
+def get_all_emails():
+    emails = []
+    session = SessionDebtors()
+    try:
+        # Wykonanie zapytania, które pobiera maile wszystkich klientów
+        debtors = session.query(Debtor.e_mail).all()
+
+        # Pobranie adresów email jako łańcuchy znaków
+        emails = [email[0] for email in debtors]
+    except Exception as e:
+        print("Wystąpił błąd:", e)
+    finally:
+        session.close()
+    return emails
+
+# Wywołanie funkcji pobierającej maile klientów z bazy danych
+emails = get_all_emails()
+
+# Wyświetlenie wyników
+if emails:
+    print("Maile klientów:")
+    for email in emails:
+        print(email)
+else:
+    print("Nie udało się pobrać danych z bazy.")
+
 # Dane do wysyłania emaila
+
 current_folder = os.path.dirname(os.path.abspath(__file__))
-sender_email = 'XXXXXXX' # Mail konta Zoho
-sender_password = 'XXXXXX'  # hasło konta Zoho
-# Wprowadzanie adresu e-mail odbiorcy
-recipient_email = input("Podaj adres e-mail odbiorcy: ")
-subject = "Prośba o uregulowanie płatności"
+sender_email = '#######' # Mail konta Zoho
+sender_password = '######'  # hasło konta Zoho
+subject = "Prośba o uregulowanie płatności !!!!!!!"
 
 # Wczytywanie treści wiadomości z pliku "prośba.docx"
 file_path = os.path.join(current_folder, "prośba.docx")
@@ -53,6 +101,6 @@ body = '\n'.join([para.text for para in doc.paragraphs])
 print("Treść wiadomości z pliku:")
 print(body)
 
-# Wywołanie funkcji send_email do wysłania wiadomości
-send_email(sender_email, sender_password, recipient_email, subject, body)
-
+# Wywołanie funkcji send_email dla każdego adresu e-mail
+for recipient_email in emails:
+    send_email(sender_email, sender_password, recipient_email, subject, body)
