a8878517d8c888bcd2d68c642e8d8f68ddadb7cd
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-25 22:41:04 +0200

    English descriptions

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Tworzenie silnika bazy danych
+# Create a database engine
 engine = create_engine('sqlite:///Customers.db', echo=True)
 
-# Inicjalizacja klasy bazowej
+# Initialize the base class
 Base = declarative_base()
 
-# Definicja modelu tabeli
+# Define the table model
 class Customer(Base): 
     __tablename__ = 'Customers'
     
@@ -18,32 +18,34 @@ class Customer(Base):
     e_mail = Column(String)  
     number_bank_statement = Column(String)  
     
-# Tworzenie tabeli w bazie danych
+# Create the table in the database
 Base.metadata.create_all(engine)
 
-# Tworzenie klasy sesji
+# Create a session class
 Session = sessionmaker(bind=engine)
 
 def add_customer():
     
+    # Add a customer to the database
     session = Session()
     list_customers()
-    name = input("Wprowadź nazwę:")
-    number_phone = input("Wprowadź numer telefonu:")
-    e_mail = input("Wprowadź e-mail:")
-    number_bank_statement = input("Wprowadź numer konta bankowego tak jak jest zapisany w pliku CSV:")
+    name = input("Enter name:")
+    number_phone = input("Enter phone number:")
+    e_mail = input("Enter email:")
+    number_bank_statement = input("Enter bank account number as it appears in the CSV file:")
     
     customer = Customer(name=name, number_phone=number_phone, e_mail=e_mail, number_bank_statement=number_bank_statement) 
     session.add(customer)
     session.commit()
     session.close()
-    print("Dodano klienta biura")   
+    print("Added office customer")   
     
 
 def delete_customer():
+    # Delete a customer from the database
     session = Session()
     list_customers()
-    customer_id = input("Wprowadź ID klienta, którego chcesz usunąć:")
+    customer_id = input("Enter the ID of the customer you want to delete:")
     
     customer = session.query(Customer).filter_by(id=customer_id).first()
     
@@ -51,63 +53,65 @@ def delete_customer():
         session.delete(customer)
         session.commit()
         session.close()
-        print("Usunięto klienta biura o ID:", customer_id)
+        print("Deleted office customer with ID:", customer_id)
     else:
         session.close()
-        print("Klient o podanym ID nie istnieje.")
+        print("Customer with specified ID does not exist.")
 
 def list_customers():
+    # List all customers in the database
     session = Session()
     customers = session.query(Customer).all()
     
     if not customers:
-        print("Brak dostępnych klientów.")
+        print("No customers available.")
     else:
-        print("Lista dostępnych klientów:")
+        print("List of available customers:")
         for customer in customers:
-            print(f"ID: {customer.id}, Nazwa: {customer.name}, Numer telefonu: {customer.number_phone}, E-mail: {customer.e_mail}, Numer konta bankowego: {customer.number_bank_statement}")
+            print(f"ID: {customer.id}, Name: {customer.name}, Phone Number: {customer.number_phone}, Email: {customer.e_mail}, Bank Account Number: {customer.number_bank_statement}")
 
 def update_customer():
+    # Update a customer's information in the database
     session = Session()
     list_customers()
-    customer_id = input("Wprowadź ID klienta, którego dane chcesz zaktualizować:")
+    customer_id = input("Enter the ID of the customer whose information you want to update:")
 
     customer = session.query(Customer).filter_by(id=customer_id).first()
 
     if customer:
-        print("Wybierz pole do zaktualizowania:")
-        print("1. Nazwa")
-        print("2. Numer telefonu")
-        print("3. E-mail")
-        print("4. Numer konta bankowego")
-        choice = input("Twój wybór (1/2/3/4):")
+        print("Choose a field to update:")
+        print("1. Name")
+        print("2. Phone Number")
+        print("3. Email")
+        print("4. Bank Account Number")
+        choice = input("Your choice (1/2/3/4):")
 
         if choice == "1":
-            new_name = input("Nowa nazwa:")
+            new_name = input("New name:")
             customer.name = new_name
         elif choice == "2":
-            new_number_phone = input("Nowy numer telefonu:")
+            new_number_phone = input("New phone number:")
             customer.number_phone = new_number_phone
         elif choice == "3":
-            new_e_mail = input("Nowy e-mail:")
+            new_e_mail = input("New email:")
             customer.e_mail = new_e_mail
         elif choice == "4":
-            new_number_bank_statement = input("Nowy numer konta bankowego:")
+            new_number_bank_statement = input("New bank account number:")
             customer.number_bank_statement = new_number_bank_statement
         else:
-            print("Nieprawidłowy wybór.")
+            print("Invalid choice.")
 
         session.commit()
         session.close()
-        print("Zaktualizowano dane klienta o ID:", customer_id)
+        print("Updated customer information with ID:", customer_id)
     else:
         session.close()
-        print("Klient o podanym ID nie istnieje.")
+        print("Customer with specified ID does not exist.")
 
 
 def main():
     
-    choose = input("Dodanie - D , Usnunięcie - U , Zmiana danych - Z:").lower()
+    choose = input("Add - D , Delete - U , Update - Z:").lower()
 
     if choose == "d":
         
@@ -123,7 +127,3 @@ def main():
     
     else:
         main()
-
-main()
-
-
