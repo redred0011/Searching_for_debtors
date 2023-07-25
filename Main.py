b0bdd2abeeeb0ce2399b76f0c280af9f721d0c1a
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-25 22:14:54 +0200

    Update

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
-# Assuming the module sending_mail is correctly named send_mail (adjust accordingly if it's different)
 import sending_mail
 from search_number_account import main as search_number_account_main
 from database import main as database_main
 from sending_mail import main as sending_mail_main
+from sending_whats_app import main as sending_whats_app_main
 
 def main_function():
     print("--------------------------------")
@@ -10,16 +10,16 @@ def main_function():
     choose = input("Wysyłanie przypomnienia mailem - M?/What's app'em - W?:").lower()
     
     if choose == "m":
+        
         choose_options = input("Zmiany bazy - Z / Szukanie dłużnika - S: ").lower()
         
         if choose_options == 'z':
-            
             database_main()
         
         elif choose_options == 's':
             search_number_account_main()
             
-            send  = input("Chcesz wysłać wiadomość? Tak - T / Nie - N:").lower()
+            send = input("Chcesz wysłać wiadomość? Tak - T / Nie - N:").lower()
             
             if send == 't':
                 sending_mail_main()
@@ -27,12 +27,30 @@ def main_function():
             elif send == 'n':
                 main_function()
             else:
-                send
+                
+                
+                main_function()
 
     elif choose == "w":
-        a = input(":")
-       
         
+        choose_options = input("Zmiany bazy - Z / Szukanie dłużnika - S: ").lower()
+        
+        if choose_options == 'z':
+            database_main()
+            
+        elif choose_options == 's':
+            search_number_account_main()
+            
+            send = input("Chcesz wysłać wiadomość? Tak - T / Nie - N: ").lower()
+            
+            if send == 't':
+                sending_whats_app_main()
+                
+            elif send == 'n':
+                main_function()
+            else:
+                main_function()
+
     main_function()
 
 main_function()
