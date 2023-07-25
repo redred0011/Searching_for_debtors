e4dea71abcd38d553ac8475b165602683e8afb78
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-25 22:40:08 +0200

    English descriptions

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import sending_mail
+
 from search_number_account import main as search_number_account_main
 from database import main as database_main
 from sending_mail import main as sending_mail_main
@@ -6,12 +6,12 @@ from sending_whats_app import main as sending_whats_app_main
 
 def main_function():
     print("--------------------------------")
-    print("Szukanie firm które są zadłużone")
-    choose = input("Wysyłanie przypomnienia mailem - M?/What's app'em - W?:").lower()
+    print("Searching for indebted companies")
+    choose = input("Send reminder by email - M? / WhatsApp - W?:").lower()
     
     if choose == "m":
         
-        choose_options = input("Zmiany bazy - Z / Szukanie dłużnika - S: ").lower()
+        choose_options = input("Database changes - Z / Search for debtor - S: ").lower()
         
         if choose_options == 'z':
             database_main()
@@ -19,7 +19,7 @@ def main_function():
         elif choose_options == 's':
             search_number_account_main()
             
-            send = input("Chcesz wysłać wiadomość? Tak - T / Nie - N:").lower()
+            send = input("Do you want to send a message? Yes - T / No - N:").lower()
             
             if send == 't':
                 sending_mail_main()
@@ -27,13 +27,11 @@ def main_function():
             elif send == 'n':
                 main_function()
             else:
-                
-                
                 main_function()
 
     elif choose == "w":
         
-        choose_options = input("Zmiany bazy - Z / Szukanie dłużnika - S: ").lower()
+        choose_options = input("Database changes - Z / Search for debtor - S: ").lower()
         
         if choose_options == 'z':
             database_main()
@@ -41,7 +39,7 @@ def main_function():
         elif choose_options == 's':
             search_number_account_main()
             
-            send = input("Chcesz wysłać wiadomość? Tak - T / Nie - N: ").lower()
+            send = input("Do you want to send a message? Yes - T / No - N: ").lower()
             
             if send == 't':
                 sending_whats_app_main()
