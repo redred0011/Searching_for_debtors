51173a4ed800a1ce799f11b22430920abd9ec508
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-25 21:14:45 +0200

    Update

--- Main.py
+++ Main.py
@@ -1,12 +1,38 @@
+# Assuming the module sending_mail is correctly named send_mail (adjust accordingly if it's different)
 import sending_mail
-import search_number_account
+from search_number_account import main as search_number_account_main
+from database import main as database_main
+from sending_mail import main as sending_mail_main
 
-
-def main():
-
+def main_function():
     print("--------------------------------")
     print("Szukanie firm które są zadłużone")
     choose = input("Wysyłanie przypomnienia mailem - M?/What's app'em - W?:").lower()
     
     if choose == "m":
+        choose_options = input("Zmiany bazy - Z / Szukanie dłużnika - S: ").lower()
         
+        if choose_options == 'z':
+            
+            database_main()
+        
+        elif choose_options == 's':
+            search_number_account_main()
+            
+            send  = input("Chcesz wysłać wiadomość? Tak - T / Nie - N:").lower()
+            
+            if send == 't':
+                sending_mail_main()
+                
+            elif send == 'n':
+                main_function()
+            else:
+                send
+
+    elif choose == "w":
+        a = input(":")
+       
+        
+    main_function()
+
+main_function()
