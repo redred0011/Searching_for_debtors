57a16a3cde2113e4ed81edf8602aad538e5f7e8e
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-25 22:15:37 +0200

    Update"

--- sending_whats_app.py
+++ sending_whats_app.py
@@ -1,41 +1,76 @@
-import requests
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+import pywhatkit as kit
+import datetime
+import docx
+import time
 
-def send_whatsapp_message(access_token, phone_number, message_template):
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
-    }
+# Create a database engine and a session factory
+engine = create_engine('sqlite:///debtors.db')
+Session = sessionmaker(bind=engine)
 
-    payload = {
-        "messaging_product": "whatsapp",
-        "to": phone_number,
-        "type": "template",
-        "template": {
-            "name": message_template,
-            "language": {
-                "code": "en_US"
-            }
-        }
-    }
+# Define the Debtor class
+Base = declarative_base()
 
-    url = 'https://graph.facebook.com/v17.0/104404726062000/messages'
+class Debtor(Base):
+    __tablename__ = 'Debtors'
 
-    response = requests.post(url, headers=headers, json=payload)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    number_phone = Column(String)
+    e_mail = Column(String)
+    number_bank_statement = Column(String)
 
-    if response.status_code == 200:
-        print("Wiadomość została pomyślnie wysłana!")
-    else:
-        print("Wystąpił błąd podczas wysyłania wiadomości.")
+def get_all_phone_numbers():
+    # Create a new session
+    session = Session()
 
-if __name__ == "__main__":
-    # Tymczasowy token dostępu WhatsApp Cloud API
-    access_token = 'EAAOdJ9jBJ2oBAO9Fm4y2HZCRdeuZC6YQCyzqZAn13rb4LZCeZAmZCVpmee2Hn3Ovd6qKEXZBC1eYZBnZCZBZAHVhSwZAygEJJ7QsZAPwMTIVdcjulOy6PbPd1nKfXYvflHumYcUv0JR7ZAIZALdT0bRmA7aknySn43CKsTRz6dx42mpn3EUwhucksc6Bb8d5fkJ5d47lgpkH1Lqt0dxkwZDZD'
+    # Query the database for all phone numbers
+    phone_numbers = session.query(Debtor.number_phone).all()
 
-    # Numer telefonu odbiorcy w międzynarodowym formacie (np. "+48" dla Polski) bez "+"
-    recipient_phone_number = 'XXXXXXXXXXX'
+    # Close the session
+    session.close()
 
-    # Nazwa szablonu wiadomości.
-    message_template = 'hello_world'   
+    # Return the list of phone numbers
+    return [phone_number[0] for phone_number in phone_numbers]
 
-    # Wysłanie wiadomości
-    send_whatsapp_message(access_token, recipient_phone_number, message_template)
+def read_docx_file(file_path):
+    # Create a Document object
+    doc = docx.Document(file_path)
+
+    # Extract the text from the document
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text + "\n"
+
+    # Return the text
+    return text
+
+def main():
+    # Get the list of phone numbers from the database
+    phone_numbers = get_all_phone_numbers()
+
+    # Set the path to the .docx file
+    file_path = "prośba.docx"
+
+    # Read the text from the .docx file
+    message = read_docx_file(file_path)
+
+    # Wait for WhatsApp Web to open (increase the initial wait time to 30 seconds)
+    time.sleep(30)
+
+    # Send the message to all phone numbers
+    for phone_number in phone_numbers:
+        if not phone_number.startswith("+"):
+            print(f"Skipping invalid phone number: {phone_number}")
+            continue
+
+        # Get the current hour and minute
+        now = datetime.datetime.now()
+        current_hour = now.hour
+        current_minute = now.minute
+
+        # Send the message with a delay of 1 minute and 15 seconds
+        kit.sendwhatmsg(phone_number, message, current_hour, current_minute + 1, 15)
+
+
